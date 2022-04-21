c210947fc26761943a6190b987c35b455e249b3e
John Adeyemo <ajohnofficial2020@gmail.com>
2022-04-21 16:28:15 +0100

    Restaurant app

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+export default class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello from App.js!</h1>
+      </React.Fragment>
+    );
+  }
+}
