ddb8c54f4c15ce94382828e79accca9e81589c83
John Adeyemo <ajohnofficial2020@gmail.com>
2022-04-26 21:27:36 +0100

    Product page updated

--- src/Components/pages/ProductsPage.js
+++ src/Components/pages/ProductsPage.js
@@ -1,12 +1,41 @@
-import Footer from "../Footer"
+import Footer from "../Footer";
+
+import FriedRice from "../../images/product/jollof_fried.jpg";
+import Semo from "../../images/product/semo.png";
+import YamPottage from "../../images/product/Yam-pottage.jpg";
+import JollofRice from "../../images/product/jollof-rice.jpg";
+import YamEgg from "../../images/product/yam_egg.jpg";
+import Fufu from "../../images/product/fufu.jpg";
 
 const ProductsPage = () => {
+  const foods = [
+    { id: 1, foodname: "FriedRice", image: FriedRice, price: "$12" },
+    { id: 2, foodname: "Semo", image: Semo, price: "$20" },
+    { id: 3, foodname: "Yam Pottage", image: YamPottage, price: "$8" },
+    { id: 4, foodname: "JollofRice", image: JollofRice, price: "$10" },
+    { id: 5, foodname: "Yam & Egg", image: YamEgg, price: "$10" },
+    { id: 6, foodname: "Fufu", image: Fufu, price: "$20" },
+  ];
+
   return (
     <div>
       <h2>Products Page</h2>
+      <p className="text-center">This are the list of available foods!</p>
+      <div className="cards">
+        {foods &&
+          foods.map((food) => (
+            <div className="card-box">
+              <div className="card-image">
+                <img src={food.image} alt="" />
+              </div>
+              <p className="detail">{food.foodname}</p>
+              <p className="detail">{food.price}</p>
+            </div>
+          ))}
+      </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default ProductsPage
+export default ProductsPage;
