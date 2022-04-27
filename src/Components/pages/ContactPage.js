3653737ce2354d106b3ae5eb417c3b3c17d93e13
Samuel Ajala <samuelajala01@gmail.com>
2022-04-27 02:14:08 +0100

    completed contact form

--- src/Components/pages/ContactPage.js
+++ src/Components/pages/ContactPage.js
@@ -1,51 +1,51 @@
-// Anyone creating the form can use Formsubmit > https://formsubmit.co/
-
 import PageTitle from "../PageTitle"
 
 const ContactPage = () => {
   return (
-    <section>
+    <section id="contact-page">
       <PageTitle title={"Contact Us"} />
-      <div>
+      <div class="contact-container">
         <div className="contact-form">
           <form
             style={{ textAlign: "center" }}
-            action="https://formsubmit.co/samuelajala01@gmail.com"
+            action="https://formsubmit.co/d7fcbe04758434a6608acbab04f926d1.com"
             method="POST"
           >
-            <h4>Send Us a message</h4>
-            <span>We love to hear from you</span>
+            <h4>Send us a message</h4>
+            <p>We love to hear from you</p>
 
-            <label for="name">
-              Name
-              <input
-                type="text"
-                name="name"
-                required
-                placeholder="Enter your name"
-              />
-            </label>
-            <label for="email">
-              Email:
-              <input
-                type="email"
-                name="email"
-                required
-                placeholder="Your email address"
-              />
-            </label>
-            <label for="message">
-              Message/Comment
-              <textarea
-                name="message"
-                required
-                placeholder="Let's hear from you"
-              />
-            </label>
+            <label>Name:</label>
+            <input
+              type="text"
+              name="name"
+              required
+              placeholder="Enter your name"
+            />
+            <label>Email:</label>
+            <input
+              type="email"
+              name="email"
+              required
+              placeholder="Your email address"
+            />
+            <label>Message/Comment:</label>
+            <textarea
+              name="message"
+              required
+              placeholder="Let's hear from you"
+              rows="5"
+            ></textarea>
             <button type="submit">Send</button>
           </form>
         </div>
-        <div className="contact-info"></div>
+        <div className="contact-info">
+          <div>
+            <h4>Address</h4>
+            <p>Las Colinas Plaza</p>
+            <p>4020 N MacArthur Blvd #100</p>
+            <p>Irving, Texas 75038</p>
+          </div>
+        </div>
       </div>
     </section>
   )
