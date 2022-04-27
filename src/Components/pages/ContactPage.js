fdc4dbf7a70ad62d76350c3f9fbcf53262098e78
Samuel Ajala <samuelajala01@gmail.com>
2022-04-27 01:06:25 +0100

    added formsubmit to form

--- src/Components/pages/ContactPage.js
+++ src/Components/pages/ContactPage.js
@@ -1,6 +1,6 @@
 // Anyone creating the form can use Formsubmit > https://formsubmit.co/
 
-import PageTitle from "../PageTitle";
+import PageTitle from "../PageTitle"
 
 const ContactPage = () => {
   return (
@@ -8,15 +8,47 @@ const ContactPage = () => {
       <PageTitle title={"Contact Us"} />
       <div>
         <div className="contact-form">
-          <form style={{ textAlign: "center" }}>
+          <form
+            style={{ textAlign: "center" }}
+            action="https://formsubmit.co/samuelajala01@email.com"
+            method="POST"
+          >
             <h4>Send Us a message</h4>
             <span>We love to hear from you</span>
+
+            <label for="name">
+              Name
+              <input
+                type="text"
+                name="name"
+                required
+                placeholder="Enter your name"
+              />
+            </label>
+            <label for="email">
+              Email:
+              <input
+                type="email"
+                name="email"
+                required
+                placeholder="Your email address"
+              />
+            </label>
+            <label for="message">
+              Message/Comment
+              <textarea
+                name="message"
+                required
+                placeholder="Question/Comment"
+              />
+            </label>
+            <button type="submit">Send</button>
           </form>
         </div>
         <div className="contact-info"></div>
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default ContactPage;
+export default ContactPage
